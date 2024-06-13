8fd30492412ee604e49ef1c586463bfe3ac3219c
Romeo509 <romeoameku503@gmail.com>
2024-06-13 02:37:01 +0000

    task 10

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,12 @@
+-- Create function SafeDiv
+DELIMITER $$
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT
+BEGIN
+    IF b = 0 THEN
+        RETURN 0;
+    ELSE
+        RETURN a / b;
+    END IF;
+END$$
+DELIMITER ;
