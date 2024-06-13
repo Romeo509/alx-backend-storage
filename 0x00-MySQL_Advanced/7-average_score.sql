235fbcca4e54b1f162bf857ea2195ae7cb1de088
Romeo509 <romeoameku503@gmail.com>
2024-06-13 02:15:01 +0000

    task 7

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,47 +1,16 @@
 -- 7-init.sql
 -- Initial setup script
 
-DROP TABLE IF EXISTS corrections;
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS projects;
+DELIMITER $$
 
-CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    average_score FLOAT DEFAULT 0,
-    PRIMARY KEY (id)
-);
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
+BEGIN
+	-- Update the average_score for the specified user_id
+	UPDATE users
+	SET
+		average_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id)
+	WHERE id = user_id;
+END $$
 
-CREATE TABLE IF NOT EXISTS projects (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE IF NOT EXISTS corrections (
-    user_id INT NOT NULL,
-    project_id INT NOT NULL,
-    score INT DEFAULT 0,
-    KEY `user_id` (`user_id`),
-    KEY `project_id` (`project_id`),
-    CONSTRAINT fk_user_id FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
-    CONSTRAINT fk_project_id FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`) ON DELETE CASCADE
-);
-
-INSERT INTO users (name) VALUES ('Bob');
-SET @user_bob = LAST_INSERT_ID();
-
-INSERT INTO users (name) VALUES ('Jeanne');
-SET @user_jeanne = LAST_INSERT_ID();
-
-INSERT INTO projects (name) VALUES ('C is fun');
-SET @project_c = LAST_INSERT_ID();
-
-INSERT INTO projects (name) VALUES ('Python is cool');
-SET @project_py = LAST_INSERT_ID();
-
-INSERT INTO corrections (user_id, project_id, score) VALUES (@user_bob, @project_c, 80);
-INSERT INTO corrections (user_id, project_id, score) VALUES (@user_bob, @project_py, 96);
-
-INSERT INTO corrections (user_id, project_id, score) VALUES (@user_jeanne, @project_c, 91);
-INSERT INTO corrections (user_id, project_id, score) VALUES (@user_jeanne, @project_py, 73);
+DELIMITER ;
