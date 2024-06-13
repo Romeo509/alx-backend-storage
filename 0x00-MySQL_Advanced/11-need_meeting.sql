2147399d1586b99e1532a4b85536fbd4011d1c23
Romeo509 <romeoameku503@gmail.com>
2024-06-13 02:42:53 +0000

    task 10

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,6 @@
+-- Create view need_meeting
+CREATE VIEW need_meeting AS
+SELECT name
+FROM students
+WHERE score < 80
+  AND (last_meeting IS NULL OR last_meeting < DATE_SUB(CURDATE(), INTERVAL 1 MONTH));
