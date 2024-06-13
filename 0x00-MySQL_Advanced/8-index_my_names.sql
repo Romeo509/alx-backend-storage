7b6bfc982582507de341d8e8954127bad04e6b09
Romeo509 <romeoameku503@gmail.com>
2024-06-13 02:24:29 +0000

    task 8

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,12 +1,4 @@
 -- Create index idx_name_first on the first letter of the name column
 
-USE holberton; -- Ensure you are using the correct database
-
--- Drop the index if it exists (optional, to ensure idempotency)
-DROP INDEX IF EXISTS idx_name_first ON names;
-
--- Create the index on the first letter of the name column
-CREATE INDEX idx_name_first ON names (LEFT(name, 1));
-
--- Optionally, you can verify the index creation
-SHOW INDEX FROM names;
+CREATE INDEX idx_name_first
+ON names(name(1));
