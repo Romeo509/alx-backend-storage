9c67a43d36ee8dde381ea2d11943550969dddb83
Romeo509 <romeoameku503@gmail.com>
2024-06-20 01:09:11 +0000

    task 2

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """
 Cache module.
-Provides a Cache class for storing data in Redis.
+Provides a Cache class for storing and retrieving data in Redis.
 """
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -31,3 +31,51 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None
+            ) -> Union[str, bytes, int, float, None]:
+        """
+        Retrieve the data from Redis and optionally
+        convert it using a callable.
+
+        Args:
+            key (str): The key to retrieve.
+            fn (Optional[Callable]): The function to convert the data.
+
+        Returns:
+            Union[str, bytes, int, float, None]:
+            The retrieved data, optionally converted.
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Optional[str]:
+        """
+        Retrieve the data from Redis and convert it to a string.
+
+        Args:
+            key (str): The key to retrieve.
+
+        Returns:
+            Optional[str]: The retrieved data as
+            a string, or None if the key does not exist.
+        """
+        return self.get(key, lambda d: d.decode('utf-8'))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """
+        Retrieve the data from Redis and convert it to an integer.
+
+        Args:
+            key (str): The key to retrieve.
+
+        Returns:
+            Optional[int]: The retrieved data as
+            an integer, or None if the key does not exist.
+        """
+        return self.get(key, int)
